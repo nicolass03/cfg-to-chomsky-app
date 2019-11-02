a70092f94bf0e6f9e3bf313293663a9cd25ef50b
Murder-Nova <sebastian.correa1@correo.icesi.edu.co>
2019-11-02 14:28:59 -0500

    Template for the Chomsky algorithm

---
+++ src/model/Chomsky.java
@@ -0,0 +1,30 @@
+package model;
+
+public class Chomsky {
+	
+	public Chomsky() {
+		
+	}
+	
+	public static void removeNonTerminals() {
+		
+	}
+	
+	public static void removeNonReachable() {
+		
+	}
+	
+	public static void simulateNullProductions() {
+		
+	}
+	
+	public static void simulateUnitProductions() {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
