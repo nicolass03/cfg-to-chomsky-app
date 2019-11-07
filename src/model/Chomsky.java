a7089d00be5f94a77b741c86242da93331948b55
Murder-Nova <sebastian.correa1@correo.icesi.edu.co>
2019-11-07 10:12:53 -0500

    Function to get reachable variables finished

--- src/model/Chomsky.java
+++ src/model/Chomsky.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Hashtable;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -8,7 +9,7 @@ import java.util.Set;
 
 public class Chomsky {
 
-	private Map<Character,ArrayList> grammar;
+	private Map<Character, ArrayList<String>> grammar;
 
 	public Chomsky(Map gram) {
 		grammar = gram;
@@ -16,53 +17,47 @@ public class Chomsky {
 	}
 
 	private void convertToChomsky() {
-		removeTerminals();
-
+		// removeTerminals();
+		removeUnreachables();
 	}
 
 	/**
-	private void initGrammar(String[][] gram) {
-		grammar = new Hashtable<>();
-		for (int i = 0; i < gram.length; i++) {
-			Character var = gram[i][0].charAt(0);
-			ArrayList prods = new ArrayList<>();
-			for (int j = 1; j < gram.length; j++) {
-				prods.add(gram[i][j]);
-			}
-			grammar.put(var, prods);
-		}
-	}
+	 * private void initGrammar(String[][] gram) { grammar = new Hashtable<>(); for
+	 * (int i = 0; i < gram.length; i++) { Character var = gram[i][0].charAt(0);
+	 * ArrayList prods = new ArrayList<>(); for (int j = 1; j < gram.length; j++) {
+	 * prods.add(gram[i][j]); } grammar.put(var, prods); } }
 	 */
 	private void removeTerminals() {
-		//Initialize terms arraylist
+		// Initialize terms arraylist
 		ArrayList terms = new ArrayList<>();
-		//goes down all the entry set
-		for(Entry e : grammar.entrySet()) {
-			//gets variable and its productions
+		// goes down all the entry set
+		for (Entry e : grammar.entrySet()) {
+			// gets variable and its productions
 			Character v = (Character) e.getKey();
 			ArrayList<String> p = (ArrayList<String>) e.getValue();
-			//the loop searchs if has any terminal prod and adds it to the terms collection
-			for(String c : p) {
-				if(isStringLowerCase(c)) {
+			// the loop searchs if has any terminal prod and adds it to the terms collection
+			for (String c : p) {
+				if (isStringLowerCase(c)) {
 					terms.add(v);
 					break;
 				}
 			}
 		}
-		//Gets all variables that produce the initial term variables
+		// Gets all variables that produce the initial term variables
 		ArrayList allTerms = getTerminals(terms);
-		//get all the variables
+		// get all the variables
 		Set<Character> keysToRemove = grammar.keySet();
-		//Removes common keys, leaving only non terminal variables on the array
+		// Removes common keys, leaving only non terminal variables on the array
 		keysToRemove.removeAll(allTerms);
-		//loop that eliminates the variable and the productions in other variables that calls it
-		for(Character q : keysToRemove) {
+		// loop that eliminates the variable and the productions in other variables that
+		// calls it
+		for (Character q : keysToRemove) {
 			grammar.remove(q);
-			for(Entry e : grammar.entrySet()) {
+			for (Entry e : grammar.entrySet()) {
 				Character v = (Character) e.getKey();
 				ArrayList<String> p = (ArrayList<String>) e.getValue();
-				for(String c : p) {
-					if(c.contains(q+"")) {
+				for (String c : p) {
+					if (c.contains(q + "")) {
 						p.remove(c);
 					}
 				}
@@ -75,46 +70,77 @@ public class Chomsky {
 		/////////////////////////////////////
 		/// algorithm that adds the new terms
 		/////////////////////////////////////
-		for(Entry e : grammar.entrySet()) {
-			//gets variable and its productions
+		for (Entry e : grammar.entrySet()) {
+			// gets variable and its productions
 			Character v = (Character) e.getKey();
 			ArrayList<String> p = (ArrayList<String>) e.getValue();
-			//the loop searchs if has any terminal prod and adds it to the terms collection
+			// the loop searchs if has any terminal prod and adds it to the terms collection
 			boolean terminal = false;
-			for(String c : p) {
-				//if verified terminal already, adds the variable to the newTerms and continues with the next var
-				//get prod as array
+			for (String c : p) {
+				// if verified terminal already, adds the variable to the newTerms and continues
+				// with the next var
+				// get prod as array
 				char[] strArray = c.toCharArray();
-				//break production into symbols and goes all over it
-				for(int i = 0; i < strArray.length; i++) {
-					//if is a terminal symbol or a terminal variable, set terminal on true and wait till the
-					//prod finish
-					if(isStringLowerCase(strArray[i]+"") || terms.contains(strArray[i])) {
+				// break production into symbols and goes all over it
+				for (int i = 0; i < strArray.length; i++) {
+					// if is a terminal symbol or a terminal variable, set terminal on true and wait
+					// till the
+					// prod finish
+					if (isStringLowerCase(strArray[i] + "") || terms.contains(strArray[i])) {
 						terminal = true;
 					}
-					//if doesnt, breaks and tries with the next production
+					// if doesnt, breaks and tries with the next production
 					else {
-						terminal=false;
+						terminal = false;
 						break;
 					}
 				}
-				if(terminal) {
+				if (terminal) {
 					newTerms.add(v);
 					break;
 				}
 			}
 		}
 
-		if(terms.equals(newTerms)) {
+		if (terms.equals(newTerms)) {
 			return newTerms;
-		}
-		else {
+		} else {
 			return getTerminals(newTerms);
 		}
 	}
 
-	private static void removeNonReachable() {
+	private void removeUnreachables() {
+		
+	}
 
+	private ArrayList<Character> findReachables(ArrayList<Character> reachables) {
+		ArrayList<Character> newReachables = (ArrayList<Character>) reachables.clone();
+		ArrayList<String> productions = null;
+
+		for (int i = 0; i < reachables.size(); i++) {
+			productions = grammar.get(reachables.get(i));
+			for (int j = 0; j < productions.size(); j++) {
+				char[] units = productions.get(j).toCharArray();
+				for (int k = 0; k < units.length; k++) {
+					if (units[k] != '-') {
+						if (isStringUpperCase("" + units[k])) {
+							if (!newReachables.contains(units[k])) {
+								newReachables.add(units[k]);
+							}
+
+						}
+					}
+				}
+
+			}
+
+		}
+
+		if (reachables.equals(newReachables)) {
+			return reachables;
+		} else {
+			return findReachables(newReachables);
+		}
 	}
 
 	private static void simulateNullProductions() {
@@ -125,13 +151,25 @@ public class Chomsky {
 
 	}
 
-	private static boolean isStringLowerCase(String str){
-		//convert String to char array
+	private static boolean isStringLowerCase(String str) {
+		// convert String to char array
 		char[] charArray = str.toCharArray();
 
-		for(int i=0; i < charArray.length; i++){
-			//if any character is not in lower case, return false
-			if( !Character.isLowerCase( charArray[i] ))
+		for (int i = 0; i < charArray.length; i++) {
+			// if any character is not in lower case, return false
+			if (!Character.isLowerCase(charArray[i]))
+				return false;
+		}
+		return true;
+	}
+
+	private static boolean isStringUpperCase(String str) {
+		// converts String to char array
+		char[] charArray = str.toCharArray();
+
+		for (int i = 0; i < charArray.length; i++) {
+			// if any character is not in upper case, returns false
+			if (!Character.isUpperCase(charArray[i]))
 				return false;
 		}
 		return true;
