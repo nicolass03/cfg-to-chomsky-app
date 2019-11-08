1ea5d5c48c5945223c89882b2c63e629050c9072
Murder-Nova <sebastian.correa1@correo.icesi.edu.co>
2019-11-08 09:17:56 -0500

    Simulate nullable variables function complete

--- src/model/Chomsky.java
+++ src/model/Chomsky.java
@@ -18,8 +18,9 @@ public class Chomsky {
 	}
 
 	private void convertToChomsky() {
-		// removeTerminals();
-		removeUnreachables();
+		//removeTerminals();
+		//removeUnreachables();
+		//simulateNullProductions();
 	}
 
 	/**
@@ -38,7 +39,7 @@ public class Chomsky {
 			ArrayList<String> p = (ArrayList<String>) e.getValue();
 			// the loop searchs if has any terminal prod and adds it to the terms collection
 			for (String c : p) {
-				if (isStringLowerCase(c)) {
+				if (isStringLowerCase(c) || c.equals("-")) {
 					terms.add(v);
 					break;
 				}
@@ -66,6 +67,7 @@ public class Chomsky {
 		}
 	}
 
+	@SuppressWarnings("unchecked")
 	private ArrayList getTerminals(ArrayList terms) {
 		ArrayList newTerms = terms;
 		/////////////////////////////////////
@@ -151,8 +153,106 @@ public class Chomsky {
 		}
 	}
 
-	private static void simulateNullProductions() {
+	private void simulateNullProductions() {
+		ArrayList<Character> nullables = getNullables();
+		for(Character c : nullables) {
+			replaceNullProductions(c);
+		}
+	}
+	
+	private ArrayList<Character> getNullables(){
+		ArrayList<Character> nullables = new ArrayList<>();
+		// goes down all the entry set
+		for (Entry e : grammar.entrySet()) {
+			// gets variable and its productions
+			Character v = (Character) e.getKey();
+			ArrayList<String> p = (ArrayList<String>) e.getValue();
+			if(v != 'S') {
+				// the loop searchs if has any lambda prod and adds it to the nullables collection
+				for (String c : p) {
+					if (c.equals("-")) {
+						nullables.add(v);
+						break;
+					}
+				}				
+			}
+		}
+		
+		return getAllNullables(nullables);
+	}
+	
+	private ArrayList getAllNullables(ArrayList nullables) {
+		ArrayList<Character> newNullables = (ArrayList<Character>) nullables.clone();
 
+		for (Entry e : grammar.entrySet()) {
+			// gets variable and its productions
+			Character v = (Character) e.getKey();
+			ArrayList<String> p = (ArrayList<String>) e.getValue();
+			if(v != 'S') {
+				boolean nullable = false;
+				// the loop searchs if has any nullable prod and adds it to the null collection
+				for (String c : p) {
+					// get prod as array
+					char[] strArray = c.toCharArray();
+					// break production into symbols and goes all over it
+					for (int i = 0; i < strArray.length; i++) {
+						//if the symbol is a nullable
+						if (isStringUpperCase(strArray[i]+"") && nullables.contains(strArray[i])) {
+							//set on true
+							nullable = true;
+							break;
+						}
+						// if doesnt, breaks and tries with the next production
+					}
+					if (nullable) {
+						if(!newNullables.contains(v))
+							newNullables.add(v);
+						break;
+					}
+				}
+				
+			}
+		}
+
+		if (nullables.equals(newNullables)) {
+			return nullables;
+		} else {
+			return getAllNullables(newNullables);
+		}
+	}
+	
+	/**
+	 * Simulates productions eliminating the variable
+	 * @param variable - variable to simulate
+	 */
+	private void replaceNullProductions(Character variable) {
+		//Goes all down the grammar asking for each variable and its production
+		for(Entry e : grammar.entrySet()) {
+			//get the var
+			Character v = (Character) e.getKey();
+			//get the prods
+			ArrayList<String> p = (ArrayList<String>) e.getValue();
+			//if is the variable, remove the lambda
+			if(v == variable && p.contains("-")) 
+				p.remove("-");
+			//clone the actual prods
+			ArrayList<String> newProds = (ArrayList<String>) p.clone();
+			//goes all down the prods
+			for (String c : p) {
+				//if the production contains my variable
+				if (c.contains(variable+"")) {
+					//create the new production replacing the var with empty
+					String newProduction = c.replace(""+variable, "");
+					//if the new prod isnt empty string and if isnt already in the newProds array
+					if(!newProduction.equals("") && !p.contains(newProduction)) {
+						//add the new prod to the new array
+						newProds.add(newProduction);						
+					}
+				}
+			}
+			//change the old prods with the new prods
+			grammar.put(v, newProds);		
+		}
 	}
 
 	private static void simulateUnitProductions() {
